9a303487f9c1af67909a2ca1dfa0568235a86d37
Tejas Lolage <tejaslolage@gmail.com>
2023-06-28 22:01:43 +0900

    Fully implemented expense model

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -2,13 +2,20 @@ import 'package:uuid/uuid.dart'; // uuid is a package that generates unique ids.
 
 const uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 
-  Expense({required this.title, required this.amount, required this.date})
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
       : id = uuid
             .v4(); // uuid.v4() generates a unique id string and passes it to the id parameter.
 }
