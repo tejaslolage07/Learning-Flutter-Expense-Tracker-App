fcf58dcd9b3e0c1d62209f757d6dd1c848de9880
Tejas Lolage <tejaslolage@gmail.com>
2023-06-28 22:29:57 +0900

    Implemented LIstView.builder() functionality

---
+++ expense_tracker/lib/expenses_list.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+// List views are always scrollable by default and
+// it's .builder method will help optimize performance by only rendering items
+// that are visible or about to be visible to the user.
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({Key? key, required this.expenses}) : super(key: key);
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) => Text(expenses[index].title),
+    );
+  }
+}
